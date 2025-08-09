cead919ecfbdb1dc9a8dab2ce5bc186e6f5f01d2
vishnu-r-1995 <vishnuvjd@gmail.com>
2025-08-09 18:03:55 +0530

    Add RedisConfig java file

---
+++ location-service/src/main/java/com/example/location/config/RedisConfig.java
@@ -0,0 +1,24 @@
+package com.example.location.config;
+
+import com.example.location.redis.RedisSubscriber;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+
+    private final RedisSubscriber redisSubscriber;
+
+    @Bean
+    public RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(redisSubscriber, new ChannelTopic("location-updates"));
+        return container;
+    }
+}
