5e772941f1fe56313f52b5b5589db75a24b507d0
vishnu-r-1995 <vishnuvjd@gmail.com>
2025-08-09 19:02:02 +0530

    Update RedisConfig.java

--- location-service/src/main/java/com/example/location/config/RedisConfig.java
+++ location-service/src/main/java/com/example/location/config/RedisConfig.java
@@ -7,18 +7,31 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.connection.MessageListener;
 
 @Configuration
 @RequiredArgsConstructor
 public class RedisConfig {
 
+    @Autowired
     private final RedisSubscriber redisSubscriber;
 
     @Bean
-    public RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory) {
+    public StringRedisTemplate redisTemplate(RedisConnectionFactory factory) {
+        return new StringRedisTemplate(factory);
+    }
+
+    @Bean
+    public MessageListenerAdapter listenerAdapter() {
+        return new MessageListenerAdapter(redisSubscriber, "onMessage");
+    }
+
+    @Bean
+    public RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,
+            MessageListenerAdapter listenerAdapter) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
-        container.addMessageListener(redisSubscriber, new ChannelTopic("location-updates"));
+        container.addMessageListener(listenerAdapter, new ChannelTopic("location-updates"));
         return container;
     }
 }
