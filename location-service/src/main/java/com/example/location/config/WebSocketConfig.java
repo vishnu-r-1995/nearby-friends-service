2ed057a7905c22b1fc5906af98b85e5567e30675
vishnu-r-1995 <vishnuvjd@gmail.com>
2025-08-11 01:10:37 +0530

    Add import for Autowired in WebSocketConfig class

--- location-service/src/main/java/com/example/location/config/WebSocketConfig.java
+++ location-service/src/main/java/com/example/location/config/WebSocketConfig.java
@@ -4,12 +4,14 @@ import com.example.location.controller.LocationWebSocketHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Configuration
 @EnableWebSocket
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketConfigurer {
 
+    @Autowired
     private final LocationWebSocketHandler handler;
 
     @Override
