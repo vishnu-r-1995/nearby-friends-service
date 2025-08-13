9a7c1be834056439c09fb3c3c9f0d14d0ebb6938
vishnu-r-1995 <vishnuvjd@gmail.com>
2025-08-14 01:04:32 +0530

    Add RedisConfig class for proximity-service

---
+++ proximity-service/src/main/java/com/example/proximity/config/RedisConfig.java
@@ -0,0 +1,33 @@
+package com.example.proximity.config;
+
+import com.example.proximity.subscriber.RedisSubscriber;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public ChannelTopic topic() {
+        return new ChannelTopic("location-updates");
+    }
+
+    @Bean
+    public MessageListenerAdapter messageListener(RedisSubscriber subscriber) {
+        return new MessageListenerAdapter(subscriber);
+    }
+
+    @Bean
+    public RedisMessageListenerContainer redisContainer(RedisConnectionFactory connectionFactory,
+                                                        MessageListenerAdapter listenerAdapter,
+                                                        ChannelTopic topic) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(listenerAdapter, topic);
+        return container;
+    }
+}
