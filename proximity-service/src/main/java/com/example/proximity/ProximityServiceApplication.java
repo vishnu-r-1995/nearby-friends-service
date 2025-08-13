ca790148e884643355fcab34ec283dc53449c5dd
vishnu-r-1995 <vishnuvjd@gmail.com>
2025-08-14 00:06:39 +0530

    Add application class for proximity-service

---
+++ proximity-service/src/main/java/com/example/proximity/ProximityServiceApplication.java
@@ -0,0 +1,11 @@
+package com.example.proximity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProximityServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProximityServiceApplication.class, args);
+    }
+}
