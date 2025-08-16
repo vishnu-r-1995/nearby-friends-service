1ca3fe2826b204b9e3e3732c36d1184dacb641cd
vishnu-r-1995 <vishnuvjd@gmail.com>
2025-08-16 17:51:36 +0530

    Add lombok annotations for LocationMessage class

--- proximity-service/src/main/java/com/example/proximity/model/LocationMessage.java
+++ proximity-service/src/main/java/com/example/proximity/model/LocationMessage.java
@@ -1,11 +1,17 @@
 package com.example.proximity.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LocationMessage {
+
     private String userId;
     private double latitude;
     private double longitude;
-    private long timestamp;
 }
